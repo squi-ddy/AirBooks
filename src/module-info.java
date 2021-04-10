3e6801065b4ee6e692c66bc224cf6bdbbd263279
SquiddyPoos <liuwenkai2017@gmail.com>
2021-04-11 01:52:11 +0800

    airbooks.fxml should not be opened

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,6 @@ module AirBooks {
     opens airbooks;
     opens airbooks.controller;
     opens airbooks.model;
-    opens airbooks.fxml;
     requires javafx.controls;
     requires javafx.fxml;
 }
